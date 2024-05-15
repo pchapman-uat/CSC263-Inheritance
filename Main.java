0cd254c2757cf000881872205eaba2d34fa4b66f
Preston Chapman <pchapman82070@uat.edu>
2024-05-15 13:17:34 -0700

    Adjust Cephalopod and Squid Classes
    Moved methods and attributes to be within the parent to be used by other classes

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         animal.printInfo();
 
         // Create Cephlapod
-        Cephlapod cephlapod = new Cephlapod("Squid", 10);
+        Cephlapod cephlapod = new Cephlapod("Squid", null, 10);
         cephlapod.printInfo();
         cephlapod.swim(20);
 
@@ -33,24 +33,27 @@ class Animal {
 }
 
 class Cephlapod extends Animal{
-    public Cephlapod(String species, int age){
+    public String subSpecies;
+    public Cephlapod(String species, String subSpecies, int age){
         super(species, age);
+        this.subSpecies = subSpecies;
     }
     public void swim(float distance){
         System.out.println("I am swiming for "+distance+" meters");
     }
-}
-
-class Squid extends Cephlapod{
-    public String subSpecies;
-    public Squid(String subSpecies, int age){
-        super("Squid", age);
-        this.subSpecies = subSpecies;
-    }
-
     @Override
     public void printInfo(){
-        System.out.println("My Species is: "+this.subSpecies+" "+this.species);
-        System.out.println("My Age is: "+this.age);
+        if(subSpecies != null){
+            System.out.println("My Species is: "+this.subSpecies+" "+this.species);
+            System.out.println("My Age is: "+this.age);
+        } else {
+            super.printInfo();
+        }
+    }
+}
+// TODO: Add methods for squid class
+class Squid extends Cephlapod{
+    public Squid(String subSpecies, int age){
+        super("Squid", subSpecies, age);
     }
 }

8eae3594e92f24b718d2c987d092cbfe28f342e2
Preston Chapman <pchapman82070@uat.edu>
2024-05-15 13:06:16 -0700

    Add base animal class

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        Animal animal = new Animal("Squid", 30);
+        animal.printInfo();
+    }
+}
+class Animal {
+    public int age;
+    public String species;
+
+    public Animal(String species, int age){
+        this.age = age;
+        this.species = species;
+    }
+
+    public void printInfo(){
+        System.out.println("My Species is: "+this.species);
+        System.out.println("My Age is: "+this.age);
     }
 }

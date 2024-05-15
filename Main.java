ce2caf7e06c1902c145842123e530bd3c44f2ae6
Preston Chapman <pchapman82070@uat.edu>
2024-05-15 13:10:29 -0700

    Add cephalopod class

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
-        Animal animal = new Animal("Squid", 30);
+
+        // Create general Animal
+        Animal animal = new Animal("Cat", 30);
         animal.printInfo();
+
+        // Create Cephlapod
+        Cephlapod cephlapod = new Cephlapod("Squid", 10);
+        cephlapod.printInfo();
+        cephlapod.swim(20);
+
     }
 }
 class Animal {
@@ -17,5 +25,14 @@ class Animal {
     public void printInfo(){
         System.out.println("My Species is: "+this.species);
         System.out.println("My Age is: "+this.age);
+    }
+}
+
+class Cephlapod extends Animal{
+    public Cephlapod(String species, int age){
+        super(species, age);
+    }
+    public void swim(float distance){
+        System.out.println("I am swiming for "+distance+" meters");
     }
 }

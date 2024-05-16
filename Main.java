a5cd596c6f9b5c25766ebe31324e0eecc8a86d1b
Preston Chapman <pchapman82070@uat.edu>
2024-05-16 14:59:18 -0700

    Comments
    Added lost limb method as well

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
-
         // Create general Animal
         Animal animal = new Animal("Cat", 30);
         animal.printInfo();
@@ -21,34 +19,46 @@ public class Main {
         // Create Dumbo Octopus
         Octopus dumbo = new Octopus("Dumbo", 10);
         dumbo.printInfo();
+        dumbo.loseLimb();
     }
 }
 class Animal {
+    // Class variables
     public int age;
     public String species;
 
+    // Constructor
     public Animal(String species, int age){
         this.age = age;
         this.species = species;
     }
 
+    // Print information about the animal 
     public void printInfo(){
         System.out.println("My Species is: "+this.species);
         System.out.println("My Age is: "+this.age);
     }
 }
 
+// Cephlapod class extends the base animal class
 class Cephlapod extends Animal{
+    // Class variables
     public String subSpecies;
+    // Constructor
     public Cephlapod(String species, String subSpecies, int age){
+        // Call the constructor of the base class
         super(species, age);
+        // Set the additonal class variables
         this.subSpecies = subSpecies;
     }
+    // Swim method
     public void swim(float distance){
         System.out.println("I am swiming for "+distance+" meters");
     }
+    // This overrides the printInfo method from the base class
     @Override
     public void printInfo(){
+        // If there is a sub species, print it, otherwise print the base class info
         if(subSpecies != null){
             System.out.println("My Species is: "+this.subSpecies+" "+this.species);
             System.out.println("My Age is: "+this.age);
@@ -57,13 +67,18 @@ class Cephlapod extends Animal{
         }
     }
 }
-// TODO: Add methods for squid class
+
+// Squid class extends the Cephlapod class
 class Squid extends Cephlapod{
+    // Class variables
     public boolean withSchool = false;
+    // Constructor
     public Squid(String subSpecies, int age){
         super("Squid", subSpecies, age);
     }
+    // Join school method
     public void joinSchool(){
+        // Check if the squid is already in a school of squids, if not, join the school
         if(!this.withSchool){
             this.withSchool = true;
             System.out.println(subSpecies+" "+species+" joined a school of other squids");
@@ -71,7 +86,9 @@ class Squid extends Cephlapod{
             System.out.println("Is already in a school of squids");
         }
     }
+    // Leave school method
     public void leaveSchool(){
+        // Check if the squid is already in a school of squids, if not, leave the school
         if(this.withSchool){
             this.withSchool = false;
             System.out.println(subSpecies+""+species+" left a school of other squids");
@@ -79,15 +96,21 @@ class Squid extends Cephlapod{
             System.out.println("Is not in a school of squids");
         }
     }
+    // This overrides the printInfo method from the base class
     @Override
     public void printInfo(){
+        // Print the base class info
         super.printInfo();
+        // If the squid is in a school of squids, print that they are
         if(withSchool){
             System.out.println("I am in a school of other squids");
         } 
     }
+    // This overrides the swim method from the cephlapod class
     @Override
     public void swim(float distance){
+        // If the squid is in a school of squids, print that they are swimming and they are in a school of squids
+        // Otherwise call the parent class swim method
         if(withSchool){
             System.out.println("I am swimming for "+distance+" meters and I am in a school of other squids");
         } else {
@@ -95,9 +118,15 @@ class Squid extends Cephlapod{
         }
     }
 }
-// TODO: Add methods for Octopus class
+
+// Octopus class extends the Cephlapod class
 class Octopus extends Cephlapod{
+    // Constructor
     public Octopus(String subSpecies, int age){
         super("Octopus", subSpecies, age);
+    }
+    // Lose limb method
+    public void loseLimb(){
+        System.out.println("I lost a limb");
     }
 }
